ed7512954f0099a502d96d62798cc3ba5595ed4f
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-12-08 09:32:54 +0900

    Update Obfuscate.py

--- Obfuscate.py
+++ Obfuscate.py
@@ -1,21 +1,25 @@
 from random import randint
 
 def obfuscate(code):
-	key = "@~pl-Steve28-lq~@"
-	code = code.lower().replace("://", key).split("/")
-	init, code = code[0].replace(key, "://"), code[1:]
-	rescode = ""
-	for block in code:
-		rescode += "/"
-		for i in list(block): rescode += chr((97+(ord(i)+1)%7) + 7*randint(0, 3))
-	return init + rescode
+    key = "@~pl-Steve28-lq~@"
+    code = code.lower().replace("://", key).split("/")
+    init, code = code[0].replace(key, "://"), code[1:]
+    rescode = ""
+    for block in code:
+        rescode += "/"
+        for i in list(block): 
+            if i in ".": rescode += i
+            else: rescode += chr((97+(ord(i)+1)%7) + 7*randint(0, 3))
+    return init + rescode
 
 def deobfuscate(code):
-	key = "@~pl-Steve28-lq~@"
-	code = code.lower().replace("://", key).split("/")
-	init, code = code[0].replace(key, "://"), code[1:]
-	rescode = ""
-	for block in code:
-		rescode += "/"
-		for i in list(block): rescode += chr(97+(ord(i)+1)%7)
-	return init + rescode
+    key = "@~pl-Steve28-lq~@"
+    code = code.lower().replace("://", key).split("/")
+    init, code = code[0].replace(key, "://"), code[1:]
+    rescode = ""
+    for block in code:
+        rescode += "/"
+        for i in list(block):
+            if i in ".": rescode += i
+            else: rescode += chr(97+(ord(i)+1)%7)
+    return init + rescode
