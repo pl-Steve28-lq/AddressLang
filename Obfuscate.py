5ffacaefe924c2f35f1f074b9f8456eea1feaddd
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-12-08 09:28:16 +0900

    Update Obfuscate.py

--- Obfuscate.py
+++ Obfuscate.py
@@ -1,6 +1,6 @@
 from random import randint
 
-def obfuscator(code):
+def obfuscate(code):
 	key = "@~pl-Steve28-lq~@"
 	code = code.lower().replace("://", key).split("/")
 	init, code = code[0].replace(key, "://"), code[1:]
@@ -8,4 +8,14 @@ def obfuscator(code):
 	for block in code:
 		rescode += "/"
 		for i in list(block): rescode += chr((97+(ord(i)+1)%7) + 7*randint(0, 3))
+	return init + rescode
+
+def deobfuscate(code):
+	key = "@~pl-Steve28-lq~@"
+	code = code.lower().replace("://", key).split("/")
+	init, code = code[0].replace(key, "://"), code[1:]
+	rescode = ""
+	for block in code:
+		rescode += "/"
+		for i in list(block): rescode += chr(97+(ord(i)+1)%7)
 	return init + rescode
